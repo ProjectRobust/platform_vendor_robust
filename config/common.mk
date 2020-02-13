b888164a1bcf1a1aa40020b3ab9385f61c304500
Dyneteve <dyneteve@pixelexperience.org>
2020-02-13 17:44:11 +0000

    robust: Include our kernel/headers generator
    * Also fix some derps

--- config/common.mk
+++ config/common.mk
@@ -16,3 +16,6 @@
 
 # Inherit from our versioning
 $(call inherit-product, vendor/robust/config/versioning.mk)
+
+# Inherit from our kernel/header generator
+$(call inherit-product, vendor/robust/config/BoardConfigRobust.mk)
